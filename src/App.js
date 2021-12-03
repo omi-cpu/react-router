3755ec3edd521ef3c0f897cd5903cf37966c53db
omi-cpu <obriggs53@gmail.com>
2021-12-03 11:17:38 +0100

    routing and link navigation

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import Contact from "./components/contact";
+import About from "./components/about";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+    return(
+        <div className="ui raised very padded text container segment" style={{marginTop: '80px'}}>
+            <h3 className="ui header">Contact</h3>
+            <p>ffftftfu ftftfu, yfyufuyfyffu</p>
+        </div>
+    )
+}
+
+export default Contact;
